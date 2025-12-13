1b8da2a813b33467da11424eda6f0c53c7b65ed2
tanishmundra-codes <tanishmundra2@gmail.com>
2025-12-13 22:44:27 +0530

    UnionandAny

---
+++ src/UnionAndAny.ts
@@ -0,0 +1,19 @@
+let subs: number | string = '1M';
+
+let apiReq: 'pending' | 'success' | 'error' = 'pending';
+
+apiReq = 'pending'
+
+const orders = ['12', '20', '65'];
+
+let currOrders: string | undefined;
+
+for(let order of orders){
+    if(order === '12'){
+        currOrders = order;
+        break;
+    }
+    currOrders = '0';
+}
+
+console.log(currOrders);
